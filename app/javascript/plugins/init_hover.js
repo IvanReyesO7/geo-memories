424b2a522e0c55093b1025c7b12f1ead6aec5604
Ivan Reyes <ivanwilf@hotmail.com>
2021-03-02 11:44:43 +0900

    fixed map images bug

--- app/javascript/plugins/init_hover.js
+++ app/javascript/plugins/init_hover.js
@@ -4,15 +4,14 @@ const initHover = () => {
   if (cards){
     const Hover = (event) =>{
       const marker = document.getElementById(`marker-${event.currentTarget.id}`)
-      marker.style.transition = "all 0.5s";
-      marker.style.width = '100px';
+      marker.style.transition = "width 0.5s, height 0.5s";
       marker.style.height = '100px';
+      marker.style.width = '100px';
       marker.style.position="absolute";
       marker.style.zIndex = "1"
       };
     const HoverOut = (event) =>{
       const marker = document.getElementById(`marker-${event.currentTarget.id}`)
-      marker.style.transition = "all 0.5s";
       marker.style.width = '25px';
       marker.style.height = '25px';
       marker.style.zIndex = "0"

8e6f299c7c4bb0b10e0ad6359570197e76d107ae
Ivan Reyes <ivanwilf@hotmail.com>
2021-03-02 21:05:45 +0900

    It locatess youu

--- app/javascript/packs/current_location.js
+++ app/javascript/packs/current_location.js
@@ -1,22 +1,21 @@
 function geoFindMe() {
 
-    const status = document.querySelector('#status');
     const mapLink = document.querySelector('#map-link');
     const location = document.querySelector('#geomem_location');
     const coordinates = document.querySelector('#geomem_coordinates');
-  
-    mapLink.href = '';
-    mapLink.textContent = '';
-  
+    
     function success(position) {
       const latitude  = position.coords.latitude;
       const longitude = position.coords.longitude;
 
-  
+      fetch(`https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${latitude}&lon=${longitude}`)
+      .then(response => response.json())
+      .then((data) => {
+        console.log(data);
+        location.value = `${data.address.city}, ${data.address.country}`;
+      });
       coordinates.value = `${longitude},${latitude}`
-      status.textContent = '';
-      mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
-      mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
+
     }
   
     function error() {
@@ -29,7 +28,7 @@ function geoFindMe() {
       location.value = 'Locating…';
       navigator.geolocation.getCurrentPosition(success, error);
     }
-  
+
   }
   
   document.querySelector('#find-me').addEventListener('click', geoFindMe);
